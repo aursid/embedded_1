bc4afcdb423683d402df73e340c12b291d7365db
JR <JR>
2017-02-12 20:39:13 +0000

    afraz's json shit

--- myfile.py
+++ myfile.py
@@ -1,13 +1,19 @@
-from machine import Pin, I2C
 import utime
+import json
+import network
+from umqtt.simple import MQTTClient
+import machine
+import time
+from machine import Pin,I2C
 
 i2c = I2C(scl = Pin(5),sda = Pin(4),freq = 500000)
 addr = i2c.scan()[0]
 
-#Declaring the registers to read data from or write data to.
+#---------Declaring the registers to read data from or write data to.-----------
 
 CTRL_REG1 = 0x20 #Control the sampling rate and take the board off low power mode.
-STATUS_REG = 0x27
+LIS3DH_REG_CTRL4 = 0x23
+STATUS_REG = 0x27 #Displays the status of the boards operation
 OUT_X_L = 0x28 #LsB of X-Axis
 OUT_X_H = 0x29 #MsB of X-Axis
 OUT_Y_L = 0x2A #LsB of Y-Axis
@@ -15,45 +21,37 @@ OUT_Y_H = 0x2B #MsB of Y-Axis
 OUT_Z_L = 0x2C #LsB of Z-Axis
 OUT_Z_H = 0x2D #MsB of Z-Axis
 
-LIS3DH_REG_CTRL4 = 0x23
-
-#range
-RANGE_16G = 48
-RANGE_8G = 32
-RANGE_4G = 16
-RANGE_2G = 0
-
-READING_SIZE_IN_BITS = 16
 
 #Take the board off low power mode.
-i2c.writeto_mem(addr,CTRL_REG1,bytearray([71]))
+i2c.writeto_mem(addr,CTRL_REG1,bytearray([71])) #71 means 1 Hz Sampling rate.
 
 #See if the board has been taken off low power mode (change later)
-print(i2c.readfrom_mem(addr,CTRL_REG1,1))
+#print(i2c.readfrom_mem(addr,CTRL_REG1,1)) #Uncomment line for CTRL_REG1 value
 
 #The Most Significant Hex indicates if Data has been overwritten
 #Lease Significant Hex indicates if new Z,Y,X data is available (in that order)
-print("Status Register is:" ,i2c.readfrom_mem(addr,STATUS_REG,1))
 
 #set the range
-set_range = RANGE_16G
-print("set_range is: ", set_range)
-i2c.writeto_mem(addr,LIS3DH_REG_CTRL4,bytearray([set_range]))
+set_range = 0
+set_range1 = set_range * 16
+i2c.writeto_mem(addr,LIS3DH_REG_CTRL4,bytearray([set_range1]))
 
-#Functions to perform conversion/standardisation
+#----------Functions to perform conversion/standardisation----------------------
 
 def raw_acceleration(n_h,n_l): #This function takes in input two decimal numbers
-    return n_l + n_h * 256
+    return n_l + n_h * 256    #and shifts the bits to make out_dir_h the MsB.
 
-def twos_complement(value):
-    if (value & (1 << (READING_SIZE_IN_BITS - 1))) != 0: #if MSB is 1
-        value = value - (1 << READING_SIZE_IN_BITS)      #compute negative
-    return value
+def twos_comp(val, bits):
+    #compute the 2's compliment of int value val
+    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)        # compute negative value
+    return val
+
+#-------------Set the range and standardisation values for m/s^2----------------
 
 #get the range
 get_range = (ord(i2c.readfrom_mem(addr,LIS3DH_REG_CTRL4,1)) / 16)
 #range_acc = ord(i2c.readfrom_mem(addr,LIS3DH_REG_CTRL4,1))
-print("range is = ", get_range)
 
 divider = 1
 if (get_range == 3): divider = 1365
@@ -61,17 +59,16 @@ if (get_range == 2): divider = 4096
 if (get_range == 1): divider = 8190
 if (get_range == 0): divider = 16380
 
-#RAW DATA
+#-------------------Collecting Raw Data from the sensor-------------------------
 # X acceleration
 x_acc_l = i2c.readfrom_mem(addr,OUT_X_L,1)
 x_acc_h = i2c.readfrom_mem(addr,OUT_X_H,1)
 
 x_acc_l_ord = ord(x_acc_l)
 x_acc_h_ord = ord(x_acc_h)
-#x_acc_h_ord = 128
 
 x_acceleration = raw_acceleration(x_acc_h_ord,x_acc_l_ord)
-x_acceleration_signed = twos_complement(x_acceleration)
+x_acceleration_signed = twos_comp(x_acceleration, 16)
 
 normalised_x_acc  = x_acceleration_signed / divider
 
@@ -84,7 +81,7 @@ y_acc_h_ord = ord(y_acc_h)
 #y_acc_h_ord = 128
 
 y_acceleration = raw_acceleration(y_acc_h_ord,y_acc_l_ord)
-y_acceleration_signed = twos_complement(y_acceleration)
+y_acceleration_signed = twos_comp(y_acceleration, 16)
 
 normalised_y_acc  = y_acceleration_signed / divider
 
@@ -97,31 +94,81 @@ z_acc_h_ord = ord(z_acc_h)
 #z_acc_h_ord = 128
 
 z_acceleration = raw_acceleration(z_acc_h_ord,z_acc_l_ord)
-z_acceleration_signed = twos_complement(z_acceleration)
+z_acceleration_signed = twos_comp(z_acceleration, 16)
 
 normalised_z_acc  = z_acceleration_signed / divider
 
-#Converting the raw data
 
-#CTRL REG 1 ENABLED, Now 1Hz sampling.
-print("X = ",x_acc_h,x_acc_l)
-print("X = ",x_acc_h_ord,x_acc_l_ord)
-print("x acceleration = ",x_acceleration)
-print("x acceleration_signed = ", x_acceleration_signed)
-print("normalised_x_acc = ", normalised_x_acc)
+#------------Network Management and JSON display formatting---------------------
+#-----------------Convert to appropriate display format.------------------------
+x_acceleration = [str(normalised_x_acc),"m/s^2"]
+x = ' '.join(x_acceleration)
+y_acceleration = [str(normalised_y_acc),"m/s^2"]
+y = ' '.join(y_acceleration)
+z_acceleration = [str(normalised_z_acc),"m/s^2"]
+z = ' '.join(z_acceleration)
 
-print("Y = ",y_acc_h,y_acc_l)
-print("Y = ",y_acc_h_ord,y_acc_l_ord)
-print("y acceleration = ",y_acceleration)
-print("y acceleration_signed = ", y_acceleration_signed)
-print("normalised_y_acc = ", normalised_y_acc)
+#-------------------Networking the data-----------------------------------------
 
-print("Z = ",z_acc_h,z_acc_l)
-print("Z = ",z_acc_h_ord,z_acc_l_ord)
-print("z acceleration = ",z_acceleration)
-print("z acceleration_signed = ", z_acceleration_signed)
-print("normalised_z_acc = ", normalised_z_acc)
+#Set up the LED for control
+led = machine.Pin(16,machine.Pin.OUT)
+led.low()
 
-#x_acc = x_acc_h
-#print(x_acc)
-#x_acc.append(x_acc_l[0])
+
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+
+sta_if.connect('EEERover','exhibition')
+
+#Until it connects
+while(not sta_if.isconnected()):
+    pass
+
+#if a connection was established
+if(sta_if.isconnected()):
+    led.high()
+
+time.sleep(0.5)
+led.low()
+
+#Connecting to the MQTT Broker
+class get_current_time():
+    def __init__(self):
+        self.time = 0
+
+    def update_time(self,msg):
+        self.time = msg
+    def printcurrenttime(self):
+        print(self.time)
+
+def sub_cb(topic,msg):
+    stott_time = str(msg,'utf-8')
+    print((topic,msg))
+    currenttime.update_time(msg)
+
+
+
+client = MQTTClient('unnamed1','192.168.0.10')
+client.connect()
+currenttime = get_current_time()
+client.set_callback(sub_cb)
+client.subscribe(b'esys/time')
+client.wait_msg()
+time.sleep(1)
+
+
+#---------------The dictionary for the acceleration values.---------------------
+acc_data = {
+    'x acceleration' : x,
+    'y acceleration' : y,
+    'z acceleration' : z,
+    'time' : currenttime.time
+}
+
+payload = json.dumps(acc_data)
+
+client.publish('/unnamed1/test',bytes(payload, 'utf-8'))
+
+
+
+#topic for time, esys\time
